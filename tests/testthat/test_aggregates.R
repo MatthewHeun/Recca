f0d56c2519b93c20c6e15d5385773393967786cb
MatthewHeun <matthew.heun@me.com>
2018-04-07 16:20:53 -0400

    Now using expect_known_value and is_testing.

--- tests/testthat/test_aggregates.R
+++ tests/testthat/test_aggregates.R
@@ -5,28 +5,27 @@ library(tibble)
 library(lazyeval)
 library(matsbyname)
 library(testthat)
+library(rlang)
 
 ###########################################################
 context("Aggregates")
 ###########################################################
 
-
-
 test_that("aggregates of SUT data work as expected", {
+  expec_path <- file.path("tests", "expectations")
 
-  # testthat sets the working directory to the folder containing this file.
-  # However, the functions use_expected_data and load_expected_data
-  # make different (default) assumptions about the location of the expectations directory,
-  # namely that it is set to the
-  # Make everything work by setting the working directory to the
-  # top level directory for this project.
-
-  # Save the current working directory, to be restored later
-  cwd <- getwd()
-  # Move the working directory up two levels, to the top level of this project.
-  setwd(file.path("..", ".."))
-
-
+  if (is_testing()) {
+    # testthat sets the working directory to the folder containing this file.
+    # However, the functions use_expected_data and load_expected_data
+    # make different (default) assumptions about the location of the expectations directory,
+    # namely that it is set to the
+    # Make everything work by setting the working directory to the
+    # top level directory for this project.
+    # Save the current working directory, to be restored later
+    cwd <- getwd()
+    # Move the working directory up two levels, to the top level of this project.
+    setwd(file.path("..", ".."))
+  }
   # Define primary industries
   p_industries <- c("Resources - Crude", "Resources - NG")
 
@@ -36,7 +35,9 @@ test_that("aggregates of SUT data work as expected", {
     primary_aggregates(p_industries = p_industries, by = "Total",
                        aggregate_primary_colname = "EX_total_agg.ktoe",
                        keep_cols = c("Country", "Year", "Energy.type", "Last.stage"))
-  expect_equal(primary_total_aggregates_sut, load_expected_data("primary_total_aggregates_sut"))
+  expect_known_value(primary_total_aggregates_sut,
+                     file.path(expec_path, "expected_primary_total_aggregates_sut.rds"),
+                     update = FALSE)
   primary_product_aggregates_sut <- sutdata %>%
     primary_aggregates(p_industries = p_industries, by = "Product",
                        aggregate_primary_colname = "EX_product_agg.ktoe",
@@ -53,6 +54,8 @@ test_that("aggregates of SUT data work as expected", {
   # expect_equal(final_total_aggregates_sut$EX_agg.ktoe, c(93000, 93000, 93000, 98220))
 
 
-  # Restore the previous working directory.
-  setwd(cwd)
+  if (is_testing()) {
+    # Restore the previous working directory.
+    setwd(cwd)
+  }
 })
