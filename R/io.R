ae3c1563aa730af02f994c45875f0235ef95de4f
MatthewHeun <matthew.heun@me.com>
2018-04-16 07:59:56 -0400

    Now calculating all IO matrices.

--- R/io.R
+++ R/io.R
@@ -3,6 +3,80 @@
 # relevant to input-ouput (PSUT) analyses
 #
 
+
+#' Calculate various input-output matrices
+#'
+#' @param .sutdata a data frame of supply-use table matrices with matrices arranged in columns.
+#' @param U_colname the name of the column in \code{.sutdata} containing Use (\code{U}) matrices.
+#' @param V_colname the name of the column in \code{.sutdata} containing Make (\code{V}) matrices.
+#' @param Y_colname the name of the column in \code{.sutdata} containing final demand (\code{Y}) matrices.
+#' @param keep_cols a vector of names of columns of \code{.sutdata} to return with the output
+#' @param g_colname the name of the output column containing \code{g} vectors.
+#' \code{g} is calculated by \code{rowsums(V)}.
+#' @param y_colname the name of the output column containing \code{y} vectors.
+#' \code{y} is calculated by \code{rowsums(Y)}.
+#' @param q_colname the name of the output column containing \code{q} vectors.
+#' \code{q} is calculated by \code{rowsums(U) + y}.
+#' @param W_colname the name of the output column containing \code{W} matrices.
+#' \code{W} is calculated by \code{transpose(V) - U}.
+#' @param Z_colname the name of the output column containing \code{Z} matrices.
+#' \code{Z} is calculated by \code{U * g_hat_inv}.
+#' @param D_colname the name of the output column containing \code{D} matrices.
+#' \code{D} is calculated by \code{V * q_hat_inv}.
+#' @param C_colname the name of the output column containing \code{C} matrices.
+#' \code{C} is calculated by \code{transpose(V) * g_hat_inv}.
+#' @param A_colname the name of the output column containing \code{A} matrices.
+#' \code{A} is calculated by \code{Z * D}.
+#' @param L_ixp_colname the name of the output column containing industry-by-product L matrices.
+#' \code{L_ixp} is calculated by \code{D * L_pxp}.
+#' @param L_pxp_colname the name of the output column containing product-by-product L matrices.
+#' \code{L_pxp} is calculated by \code{(I - Z*D)^-1}.
+#'
+#' @return a data frame containing columns specified in \code{keep_cols},
+#' \code{y_colname}, \code{q_colname}, \code{g_colname}, \code{W_colname},
+#' \code{Z_colname}, \code{D_colname}, \code{C_colname}, \code{A_colname},
+#' \code{L_ixp_colname}, and \code{L_pxp_colname}.
+#'
+#' @export
+calc_io_mats <- function(.sutdata,
+                         # Input columns
+                         U_colname = "U", V_colname = "V", Y_colname = "Y",
+                         # Output columns
+                         keep_cols = NULL,
+                         y_colname = "y", q_colname = "q", g_colname = "g", W_colname = "W",
+                         Z_colname = "Z", D_colname = "D", C_colname = "C", A_colname = "A",
+                         L_ixp_colname = "L_ixp", L_pxp_colname = "L_pxp"){
+  .sutdata %>%
+    # Clean the matrices in the columns.
+    # This step avoids situations where rows or columns of zeroes
+    # cause a _hat_inv step to fail due to inverting a matrix with a row or column of zeroes.
+    mutate_(
+      .dots = list(
+        # U = clean_byname(U)
+        interp(~ clean_byname(ucol, margin = c(1,2), clean_value = 0),
+               ucol = as.name(U_colname)),
+        # V = clean_byname(V)
+        interp(~ clean_byname(vcol, margin = c(1,2), clean_value = 0),
+               vcol = as.name(V_colname)),
+        # Y = clean_byname(Y)
+        interp(~ clean_byname(ycol, margin = c(1,2), clean_value = 0),
+               ycol = as.name(Y_colname))
+      ) %>%
+        setNames(c(U_colname, V_colname, Y_colname))
+    ) %>%
+    calc_yqgW(U_colname = U_colname, V_colname = V_colname, Y_colname = Y_colname, W_colname = W_colname,
+              keep_cols = c(keep_cols, U_colname, V_colname),
+              y_colname = y_colname, q_colname = q_colname, g_colname = g_colname) %>%
+    calc_A(U_colname = U_colname, V_colname = V_colname, q_colname = q_colname, g_colname = g_colname,
+           keep_cols = c(keep_cols, y_colname, q_colname, g_colname, W_colname),
+           Z_colname = Z_colname, D_colname = D_colname, C_colname = C_colname, A_colname = A_colname) %>%
+    calc_L(D_colname = D_colname, A_colname = A_colname,
+           keep_cols = c(keep_cols, y_colname, q_colname, g_colname, W_colname, Z_colname, D_colname, C_colname, A_colname),
+           L_ixp_colname = L_ixp_colname, L_pxp_colname = L_pxp_colname)
+}
+
+
+
 #' Calculate y, g, and q vectors and W matrices
 #'
 #' @param .sutdata a data frame of supply-use table matrices with matrices arranged in columns.
@@ -145,4 +219,46 @@ calc_A <- function(.sutdata,
         setNames(A_colname)
     ) %>%
     select_(.dots = c(intersect(keep_cols, names(.)), Z_colname, C_colname, D_colname, A_colname))
+}
+
+
+#' Calculates total requirements matrices
+#'
+#' @param .sutdata a data frame of supply-use table matrices with matrices arranged in columns.
+#' @param D_colname the name of the column in \code{.sutdata} containing the \code{D} matrix.
+#' @param A_colname the name of the column in \code{.sutdata} containing the \code{A} matrix.
+#' @param keep_cols a vector of names of columns of \code{.sutdata} to return with the output
+#' @param L_ixp_colname the name of the output column containing the industry-by-product L matrix.
+#' \code{L_ixp} is calculated by \code{D * L_pxp}.
+#' @param L_pxp_colname the name of the output column containing the product-by-product L matrix.
+#' \code{L_pxp} is calculated by \code{(I - Z*D)^-1}.
+#'
+#' @return a data frame containing columns specified in \code{keep_cols},
+#' \code{L_ixp_colname}, and \code{L_pxp_colname}.
+calc_L <- function(.sutdata,
+                   # Input columns
+                   D_colname = "D", A_colname = "A",
+                   # Output columns
+                   keep_cols = NULL,
+                   L_ixp_colname = "L_ixp", L_pxp_colname = "L_pxp"){
+  .sutdata %>%
+    select_(.dots = c(intersect(keep_cols, names(.)), D_colname, A_colname)) %>%
+    mutate_(
+      .dots = list(
+        # L_pxp = (I - A)^-1
+        interp(~ Iminus_byname(bdcol) %>% invert_byname(),
+               bdcol = as.name(A_colname))
+      ) %>%
+        setNames(c(L_pxp_colname))
+    ) %>%
+    mutate_(
+      .dots = list(
+        # L_ixp = D * L_pxp
+        interp(~ matrixproduct_byname(dcol, ppcol),
+               dcol = as.name(D_colname),
+               ppcol = as.name(L_pxp_colname))
+      ) %>%
+        setNames(c(L_ixp_colname))
+    ) %>%
+    select_(.dots = c(intersect(keep_cols, names(.)), L_pxp_colname, L_ixp_colname))
 }
