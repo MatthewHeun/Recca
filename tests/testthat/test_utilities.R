7a17ae624ee94062eef34f9e6d05c205e96c4e6c
MatthewHeun <matthew.heun@me.com>
2018-04-22 19:26:49 -0400

    Added check_colnames function

---
+++ tests/testthat/test_utilities.R
@@ -0,0 +1,19 @@
+library(Hmisc)
+
+###########################################################
+context("Utilities")
+###########################################################
+
+test_that("error messages about column names works as expected", {
+  df <- data.frame(a = c(1,2), b = c(3,4))
+  newcols <- c("c", "d", "a", "b")
+  expect_error(check_colnames(df, newcols),
+               Hmisc::escapeRegex("column(s) 'a', 'b' is (are) already column names in data frame 'df'"))
+
+  expect_silent(check_colnames(df, c("d")))
+
+})
+
+
+
+
