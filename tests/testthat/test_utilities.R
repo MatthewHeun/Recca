acfb0b39b9c7b57503c6c31455f0f6c73f9fc717
MatthewHeun <matthew.heun@me.com>
2018-05-12 16:13:24 -0400

    Deleted ensure_DF test. Function gone.

--- tests/testthat/test_utilities.R
+++ tests/testthat/test_utilities.R
@@ -36,19 +36,3 @@ test_that("verify_cols_missing works with a single value", {
   expect_error(verify_cols_missing(df, as.name("a")),
                Hmisc::escapeRegex("column(s) 'a' is (are) already column names in data frame 'df'"))
 })
-
-test_that("ensure_DF works as expected", {
-  m1 <- matrix(c(1, 1), nrow = 2, ncol = 1, dimnames = list(c("r1", "r2"), "c1"))
-  m2 <- matrix(c(42, 42), nrow = 2, ncol = 1, dimnames = list(c("r1", "r2"), "c1"))
-  df <- data.frame(v1 = c(1,2), v2 = c(3,4))
-
-  expect_error(ensure_DF(df, m1 = m1, m2 = m2), "All ... arguments were matrices, but .DF was not missing or NULL in ensure_DF")
-  expect_error(ensure_DF(U = "U", V = "V"), "All ... arguments were character, but .DF was not a data frame in ensure_DF")
-  expect_error(ensure_DF(df, a = 5), "Unexpected inputs to ensure_DF")
-  expect_error(ensure_DF(NULL, m1 = m1, m2 = m2, m3 = 5), "Unexpected inputs to ensure_DF")
-
-  expectedDF <- data.frame(m1 = I(list(m1)), m2 = I(list(m2)))
-  expect_equal(ensure_DF(NULL, m1 = m1, m2 = m2), expectedDF)
-  expect_equal(ensure_DF(m1 = m1, m2 = m2), expectedDF)
-})
-
