7d92cf62351a185cbd33eaf87cac54fa8f170648
MatthewHeun <matthew.heun@me.com>
2018-06-10 21:16:45 -0400

    Delete unneeded (commented) code.

--- R/aggregates.R
+++ R/aggregates.R
@@ -241,29 +241,6 @@ primary_aggregates_IEA <- function(.ieadata,
   .ieadata %>%
     filter(starts_with_any_of(!!flow, p_industries), !startsWith(!!flow_aggregation_point, eiou)) %>%
     summarise(!!aggregate_primary_colname := sum(!!energy))
-  # .ieadata %>%
-  #   filter_(interp(~ uvy %in% c(vp, gp),
-  #                  uvy = as.name(UVY_colname),
-  #                  vp = V_p_varname,
-  #                  gp = Y_p_varname)
-  #   ) %>%
-  #   group_by_(.dots = c(UVY_colname), add = TRUE) %>%
-  #   summarise_(.dots = lazyeval::interp(~ sum(EX), EX = as.name(energy_colname)) %>%
-  #                list %>%
-  #                setNames(energy_colname)
-  #   ) %>%
-  #   spread_(key_col = UVY_colname, value_col = energy_colname) %>%
-  #   mutate_(
-  #     # Note that the calculation below is a sum, because IEA data are signed;
-  #     # V_p data are positive, and Y_p data are negative, because exports are viewed as negative supply.
-  #     # So, we can simply add these items here.
-  #     .dots = interp(~ vp + gp,
-  #                    vp = as.name(V_p_varname),
-  #                    gp = as.name(Y_p_varname)) %>%
-  #       list %>%
-  #       setNames(aggregate_primary_colname)
-  #   ) %>%
-  #   select_(.dots = c(groups(.), aggregate_primary_colname))
 }
 
 
@@ -322,14 +299,6 @@ primary_aggregates_IEA <- function(.ieadata,
 #'   group_by(Country, Year, Energy.type, Last.stage) %>%
 #'   finaldemand_aggregates_IEA()
 finaldemand_aggregates_IEA <- function(.ieadata,
-                                       # # Input information
-                                       # UVY_colname = "UVY", energy_colname = "EX.ktoe",
-                                       # Y_fd_varname = "Y_fd", Y_ne_varname = "Y_ne",
-                                       # U_EIOU_varname = "U_EIOU",
-                                       # # Output information
-                                       # aggregate_net_finaldemand_colname,
-                                       # aggregate_gross_finaldemand_colname){
-
                                        # Input information
                                        ledger_side = "Ledger.side",
                                        flow_aggregation_point = "Flow.aggregation.point",
@@ -369,47 +338,5 @@ finaldemand_aggregates_IEA <- function(.ieadata,
       !!as.name(aggregate_gross_finaldemand_colname) := !!as.name(aggregate_net_finaldemand_colname) + !!diff_colname
     ) %>%
     select(-(!!diff_colname))
-
-  # # First calculate net energy
-  # .ieadata %>%
-  #   filter_(.dots = interp(~ uvy %in% c(Y_fd_varname, Y_ne_varname),
-  #                          uvy = as.name(UVY_colname))) %>%
-  #   summarise_(.dots = interp(~ sum(EX), EX = as.name(energy_colname)) %>%
-  #                list %>%
-  #                setNames(aggregate_net_finaldemand_colname)
-  #   ) %>%
-  #   full_join(
-  #     # Combine with EIOU energy
-  #     .ieadata %>%
-  #       filter_(lazyeval::interp(~ uvy == ueiou,
-  #                                uvy = as.name(UVY_colname),
-  #                                ueiou = U_EIOU_varname)) %>%
-  #       group_by_(.dots = c(UVY_colname), add = TRUE) %>%
-  #       # summarise(EX.ktoe = sum(EX.ktoe)) %>%
-  #       summarise_(.dots = interp(~ sum(EX), EX = as.name(energy_colname)) %>%
-  #                    list %>%
-  #                    setNames(".U_EIOU")
-  #       ) %>%
-  #       select_(.dots = lazyeval::interp(~ - uvy,
-  #                                        uvy = as.name(UVY_colname))
-  #       ),
-  #     by = as.character(groups(.ieadata))
-  #   ) %>%
-  #   mutate_(
-  #     # Gross energy is net - eiou
-  #     # Note that eiou numbers are negative (because EIOU decreases supply in Transformation processes).
-  #     # However, we want to add EIOU to the net energy.
-  #     # So, we need to subtract EIOU from net below.
-  #     .dots = interp(~ net - eiou,
-  #                    net = as.name(aggregate_net_finaldemand_colname),
-  #                    eiou = as.name(".U_EIOU")) %>%
-  #       list %>%
-  #       setNames(aggregate_gross_finaldemand_colname)
-  #   ) %>%
-  #   select_(
-  #     # Get rid of .U_EIOU column
-  #     .dots = interp(~ - eiou,
-  #                    eiou = as.name(".U_EIOU"))
-  #   )
 }
 
