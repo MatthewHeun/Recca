8861dfcde771a004d815f7a87eba7766f8fcead1
MatthewHeun <matthew.heun@me.com>
2018-10-21 21:26:24 -0400

    Now includes perfect sub data in package.

--- R/data.R
+++ R/data.R
@@ -13,7 +13,7 @@
 #' 13 November 2017.
 #'
 #' \code{UKEnergy2000tidy} gives each non-zero entry in \code{UKEnergy2000mats}
-#' as a single row in a data frame.
+#' as a single column in a data frame.
 #' These data are in tidy format.
 #'
 #' @format A data frame with 186 rows and 9 variables:
@@ -33,6 +33,7 @@
 "UKEnergy2000tidy"
 
 
+
 #' Energy consumption in the UK in 2000
 #'
 #' A dataset containing approximations to
@@ -47,7 +48,7 @@
 #' Leeds, England,
 #' 13 November 2017.
 #'
-#' \code{UKEnergy2000mats} gives the make (\code{V}), use (\code{U}), and final demand (\code{Y}) matrices
+#' \code{UKEnergy2000mats} gives the use (\code{U}), make (\code{V}), and final demand (\code{Y}) matrices
 #' associated with \code{UKEnergy2000tidy}.
 #' These data are in \pkg{matsindf} format.
 #'
@@ -62,3 +63,53 @@
 #' }
 #' @source \url{http://www.see.leeds.ac.uk/fileadmin/Documents/research/sri/workingpapers/sri-wp111.pdf}
 "UKEnergy2000mats"
+
+
+
+#' Example energy conversion chain to demonstrate perfect substitution
+#'
+#' A dataset containing an example energy conversion chain
+#' for the purpose of demonstrating changes to industry inputs
+#' where the inputs are perfect substitutes.
+#'
+#' \code{PerfectSubtidy} gives each non-zero entry in \code{PerfectSubmats}
+#' as a single column in a data frame.
+#' These data are in tidy format.
+#'
+#' @format A data frame with 20 rows and 10 variables:
+#' \describe{
+#'   \item{Country}{country, (Example, only one country)}
+#'   \item{Year}{year, 2000 (only one year as an example)}
+#'   \item{Ledger.side}{Supply or Consumption}
+#'   \item{Flow.aggregation.point}{tells where each row should be aggregated}
+#'   \item{Energy.type}{E.ktoe (for energy) or X.ktoe (for exergy)}
+#'   \item{Last.stage}{tells the final stage of the energy conversion chain: services is the only entry here}
+#'   \item{Flow}{the Industry or Sector involved in this flow}
+#'   \item{Product}{the energy product involved in this flow}
+#'   \item{EX.ktoe}{value of the energy, exergy, or service flow in ktoe}
+#'   \item{Unit}{unit in which quantity is expressed}
+#' }
+"PerfectSubtidy"
+
+
+
+#' Example energy conversion chain to demonstrate perfect substitution
+#'
+#' A dataset containing an example energy conversion chain
+#' for the purpose of demonstrating changes to industry inputs
+#' where the inputs are perfect substitutes.
+#'
+#' \code{PerfectSubmats} gives the use (\code{U}), make (\code{V}), and final demand (\code{Y}) matrices
+#' as a single column in a data frame.
+#' These data are in \pkg{matsindf} format.
+#'
+#' @format A data frame with 20 rows and 10 variables:
+#' \describe{
+#'   \item{Country}{country, (Example, only one country)}
+#'   \item{Year}{year, 2000 (only one year as an example)}
+#'   \item{Energy.type}{E.ktoe (for energy) or X.ktoe (for exergy)}
+#'   \item{Last.stage}{tells the final stage of the energy conversion chain: services is the only entry here}
+#'   \item{matrix.name}{gives the name of the matrix}
+#'   \item{matrix}{gives use (U), make (V), final demand (Y), r_EIOU, and S_units matrices}
+#' }
+"PerfectSubmats"
--- tests/testthat/test_perfectsubstitution.R
+++ tests/testthat/test_perfectsubstitution.R
@@ -11,50 +11,7 @@ library(tidyr)
 context("Perfect substitution")
 ###########################################################
 
-prep_perfect_subsitution <- function(){
-  PerfectSubtidy <- read.csv(system.file("extdata", "PerfectSubstitutionraw", "PerfectSubstitutionraw.csv",
-                                           package = "Recca", mustWork = TRUE),
-                               stringsAsFactors = FALSE)
-
-  # Create S_units matrices from the PerfectSubtidy data frame
-  S_units <- PerfectSubtidy %>%
-    group_by(Country, Year, Energy.type, Last.stage) %>%
-    S_units_from_tidy()
-
-  PerfectSubmats <- PerfectSubtidy %>%
-    # Add a column indicating the matrix in which this entry belongs (U, V, or Y).
-    add_matnames_iea() %>%
-    # Add metadata columns for row names, column names, row types, and column types.
-    add_row_col_meta() %>%
-    # Eliminate columns we no longer need
-    select(-Ledger.side, -Flow.aggregation.point, -Flow, -Product) %>%
-    mutate(
-      # Ensure that all energy values are positive, as required for analysis.
-      EX.ktoe = abs(EX.ktoe)
-    ) %>%
-
-    # Collapse to matrices
-    group_by(Country, Year, Energy.type, Last.stage, matname) %>%
-    collapse_to_matrices(matnames = "matname", matvals = "EX.ktoe",
-                         rownames = "rowname", colnames = "colname",
-                         rowtypes = "rowtype", coltypes = "coltype") %>%
-    rename(matrix.name = matname, matrix = EX.ktoe) %>%
-    spread(key = matrix.name, value = matrix) %>%
-
-    mutate(
-      # Create full U matrix
-      U = sum_byname(U_excl_EIOU, U_EIOU),
-      r_EIOU = elementquotient_byname(U_EIOU, U),
-      r_EIOU = replaceNaN_byname(r_EIOU, val = 0)
-    ) %>%
-    select(-U_EIOU, -U_excl_EIOU) %>%
-    # Add S_units matrices
-    left_join(S_units, by = c("Country", "Year", "Energy.type", "Last.stage"))
-
-  return(list(tidy = PerfectSubtidy, mats = PerfectSubmats))
-}
-
-test_that("calculation of B matrix works", {
+test_that("calculation of K matrix works", {
   expec_path <- file.path("tests", "expectations")
 
   if (is_testing()) {
@@ -68,9 +25,8 @@ test_that("calculation of B matrix works", {
     setwd(file.path("..", ".."))
   }
 
-  perfectsublist <- prep_perfect_subsitution()
-  perfectsub_tidy <- perfectsublist$tidy
-  perfectsub_mats <- perfectsublist$mats
+  perfectsub_mats <- PerfectSubmats %>%
+    spread(key = "matrix.name", value = "matrix")
 
   io_mats <- perfectsub_mats %>% calc_io_mats()
   K <- io_mats$K[[1]]
