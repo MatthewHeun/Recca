a1ba89b3ec894bf2b96ce02e27d523218a46625c
MatthewHeun <matthew.heun@me.com>
2020-03-18 21:01:03 -0400

    Eliminating .csv files. Reading from .xlsx files directly.

--- data-raw/prep_perfectsub.R
+++ data-raw/prep_perfectsub.R
@@ -19,37 +19,36 @@ library(matsbyname)
 library(matsindf)
 library(devtools)
 
-# Load the raw data from the .csv file
-PerfectSubtidy <- read.csv(system.file("extdata", "PerfectSubstitutionraw", "PerfectSubstitutionraw.csv",
-                                       package = "Recca", mustWork = TRUE),
-                           stringsAsFactors = FALSE)
+# Load the raw data from the .Excel file
+PerfectSubtidy <- openxlsx::read.xlsx(system.file("extdata", "PerfectSubstitutionraw", "UTEI_Sankey_Simple_ECC_Perfect_Sub.xlsx",
+                                                    package = "Recca", mustWork = TRUE), sheet = "PerfectSubstitutionRaw")
 
-use_data(PerfectSubtidy, overwrite = TRUE)
+usethis::use_data(PerfectSubtidy, overwrite = TRUE)
 
 # Create S_units matrices from the PerfectSubtidy data frame
 S_units <- PerfectSubtidy %>%
-  group_by(Country, Year, Energy.type, Last.stage) %>%
+  dplyr::group_by(Country, Year, Energy.type, Last.stage) %>%
   S_units_from_tidy()
 
 PerfectSubmats <- PerfectSubtidy %>%
   # Add a column indicating the matrix in which this entry belongs (U, V, or Y).
   add_matnames_iea() %>%
   # Add metadata columns for row names, column names, row types, and column types.
-  add_row_col_meta() %>%
+  IEATools::add_row_col_meta() %>%
   # Eliminate columns we no longer need
-  select(-Ledger.side, -Flow.aggregation.point, -Flow, -Product) %>%
-  mutate(
+  dplyr::select(-Ledger.side, -Flow.aggregation.point, -Flow, -Product) %>%
+  dplyr::mutate(
     # Ensure that all energy values are positive, as required for analysis.
     EX.ktoe = abs(EX.ktoe)
   ) %>%
 
   # Collapse to matrices
-  group_by(Country, Year, Energy.type, Last.stage, matname) %>%
-  collapse_to_matrices(matnames = "matname", matvals = "EX.ktoe",
-                       rownames = "rowname", colnames = "colname",
-                       rowtypes = "rowtype", coltypes = "coltype") %>%
-  rename(matrix.name = matname, matrix = EX.ktoe) %>%
-  spread(key = matrix.name, value = matrix) %>%
+  dplyr::group_by(Country, Year, Energy.type, Last.stage, matname) %>% View
+  matsindf::collapse_to_matrices(matnames = "matnames", matvals = "E.dot",
+                                 rownames = "rownames", colnames = "colnames",
+                                 rowtypes = "rowtypes", coltypes = "coltypes") %>%
+  dplyr::rename(matrix.name = matname, matrix = EX.ktoe) %>%
+  tidyr::spread(key = matrix.name, value = matrix) %>%
 
   dplyr::mutate(
     # Create full U matrix
@@ -57,10 +56,10 @@ PerfectSubmats <- PerfectSubtidy %>%
     r_EIOU = quotient_byname(U_EIOU, U),
     r_EIOU = replaceNaN_byname(r_EIOU, val = 0)
   ) %>%
-  select(-U_EIOU, -U_excl_EIOU) %>%
+  dplyr::select(-U_EIOU, -U_excl_EIOU) %>%
   # Add S_units matrices
-  left_join(S_units, by = c("Country", "Year", "Energy.type", "Last.stage")) %>%
-  gather(key = matrix.name, value = matrix, U, V, Y, r_EIOU, S_units)
+  dplyr::left_join(S_units, by = c("Country", "Year", "Energy.type", "Last.stage")) %>%
+  tidyr::gather(key = matrix.name, value = matrix, U, V, Y, r_EIOU, S_units)
 
 
-use_data(PerfectSubmats, overwrite = TRUE)
+usethis::use_data(PerfectSubmats, overwrite = TRUE)
