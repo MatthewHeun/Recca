da0c336aa2c9b4fa7fbb5d3768c659c920a5f131
Matthew Heun <matthew.heun@me.com>
2022-08-15 15:37:12 -0400

    Better spacing

--- data-raw/create_constants.R
+++ data-raw/create_constants.R
@@ -99,6 +99,7 @@ energy_types <- list(energy_type = "Energy.type", # Column name
                      x = "X") # Exergy
 usethis::use_data(energy_types, overwrite = TRUE)
 
+
 #
 # High pressure matter Eu.products
 #
@@ -106,6 +107,7 @@ hpm_eu.products <- list(hpa = "HPA",
                         hpl = "HPL",
                         hpng = "HPNG")
 usethis::use_data(hpm_eu.products, overwrite = TRUE)
+
 
 #
 # High temperature heat (HTH) Eu.products
@@ -119,12 +121,14 @@ hth_eu.products <- list(hth_400_c = "HTH.400.C",
                         hth_1600_c = "HTH.1600.C")
 usethis::use_data(hth_eu.products, overwrite = TRUE)
 
+
 #
 # Medium temperature heat (MTH) Eu.products
 #
 mth_eu.products <- list(mth_100_c = "MTH.100.C",
                         mth_200_c = "MTH.200.C")
 usethis::use_data(mth_eu.products, overwrite = TRUE)
+
 
 #
 # Low temperature heat (LTH) Eu.products
@@ -133,12 +137,14 @@ lth_eu.products <- list(lth_20_c = "LTH.20.C",
                         lth_60_c = "LTH.60.C")
 usethis::use_data(lth_eu.products, overwrite = TRUE)
 
+
 #
 # Cooling Eu.products
 #
 cooling_eu.products <- list(`ltc_-10_c` = "LTC.-10.C",
                             ltc_20_c = "LTC.20.C")
 usethis::use_data(cooling_eu.products, overwrite = TRUE)
+
 
 #
 # Propulsion Eu.products
@@ -147,6 +153,7 @@ propulsion_eu.products <- list(mp = "MP",
                                rop = "RoP",
                                rap = "RaP")
 usethis::use_data(propulsion_eu.products, overwrite = TRUE)
+
 
 #
 # Mechanical Eu.products
@@ -157,11 +164,13 @@ mechanical_eu.products <- list(md = "MD",
                                mf = "MF")
 usethis::use_data(mechanical_eu.products, overwrite = TRUE)
 
+
 #
 # Information processing (IP) Eu.products
 #
 ip_eu.products <- list(ip = "IP")
 usethis::use_data(ip_eu.products, overwrite = TRUE)
+
 
 #
 # Light (L) Eu.products
@@ -169,11 +178,13 @@ usethis::use_data(ip_eu.products, overwrite = TRUE)
 l_eu.products <- list(L = "L")
 usethis::use_data(l_eu.products, overwrite = TRUE)
 
+
 #
 # Non-energy use (NEU) Eu.products
 #
 neu_eu.products <- list(neu = "NEU")
 usethis::use_data(neu_eu.products, overwrite = TRUE)
+
 
 #
 # Eu.product Aggregation information
