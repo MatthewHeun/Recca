6bcd3220294dcaabe30421e83825abaaad5736db
Matthew Heun <matthew.heun@me.com>
2023-06-23 20:57:17 -0400

    Change name of variable.

--- tests/testthat/test-pfu_aggregates.R
+++ tests/testthat/test-pfu_aggregates.R
@@ -3,7 +3,7 @@ test_that("pfu_aggregates() works as expected", {
   p_industries <- c("Resources [of Crude]", "Resources [of NG]")
 
   # Primary TOTAL aggregates
-  primary_total_aggregates_sut <- UKEnergy2000mats |>
+  pfu_aggs <- UKEnergy2000mats |>
     tidyr::pivot_wider(names_from = matrix.name, values_from = matrix) |>
     pfu_aggregates()
 
