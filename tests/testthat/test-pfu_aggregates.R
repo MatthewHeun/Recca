4c0a691414282216362c653a1be9a510af2a5f4a
Matthew Heun <matthew.heun@me.com>
2023-07-04 20:12:50 -0400

    Working on pfu_aggregates()

--- tests/testthat/test-pfu_aggregates.R
+++ tests/testthat/test-pfu_aggregates.R
@@ -3,31 +3,102 @@ test_that("pfu_aggregates() works as expected", {
   p_industries <- c("Resources [of Crude]", "Resources [of NG]")
 
   # Primary TOTAL aggregates
-  pfu_aggs <- UKEnergy2000mats |>
+  pfu_aggs_total <- UKEnergy2000mats |>
     tidyr::pivot_wider(names_from = matrix.name, values_from = matrix) |>
     pfu_aggregates(p_industries = p_industries, by = "Total")
-  pfu_aggs |>
+
+  pfu_aggs_total |>
     dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "E",
                   .data[[Recca::psut_cols$last_stage]] == "Final") |>
     magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
     magrittr::extract2(1) |>
     expect_equal(93000)
-  pfu_aggs |>
+  pfu_aggs_total |>
     dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "E",
                   .data[[Recca::psut_cols$last_stage]] == "Useful") |>
     magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
     magrittr::extract2(1) |>
     expect_equal(93000)
-  pfu_aggs |>
+  pfu_aggs_total |>
     dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "E",
                   .data[[Recca::psut_cols$last_stage]] == "Services") |>
     magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
     magrittr::extract2(1) |>
     expect_equal(93000)
-  pfu_aggs |>
+  pfu_aggs_total |>
     dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "X",
                   .data[[Recca::psut_cols$last_stage]] == "Services") |>
     magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
     magrittr::extract2(1) |>
     expect_equal(98220)
+
+
+  # Primary PRODUCT aggregates
+  pfu_aggs_product <- UKEnergy2000mats |>
+    tidyr::pivot_wider(names_from = matrix.name, values_from = matrix) |>
+    pfu_aggregates(p_industries = p_industries, by = "Product")
+
+  pfu_aggs_product |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "E",
+                  .data[[Recca::psut_cols$last_stage]] == "Final") |>
+    magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
+    magrittr::extract2(1) |>
+    expect_equal(matrix(c(50000, 43000), ncol = 1, dimnames = list(c("Crude", "NG"), "Industry")) |>
+                   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"))
+  pfu_aggs_product |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "E",
+                  .data[[Recca::psut_cols$last_stage]] == "Useful") |>
+    magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
+    magrittr::extract2(1) |>
+    expect_equal(matrix(c(50000, 43000), ncol = 1, dimnames = list(c("Crude", "NG"), "Industry")) |>
+                   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"))
+  pfu_aggs_product |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "E",
+                  .data[[Recca::psut_cols$last_stage]] == "Services") |>
+    magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
+    magrittr::extract2(1) |>
+    expect_equal(matrix(c(50000, 43000), ncol = 1, dimnames = list(c("Crude", "NG"), "Industry")) |>
+                   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"))
+  pfu_aggs_product |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "X",
+                  .data[[Recca::psut_cols$last_stage]] == "Services") |>
+    magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
+    magrittr::extract2(1) |>
+    expect_equal(matrix(c(53500, 44720), ncol = 1, dimnames = list(c("Crude", "NG"), "Industry")) |>
+                   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"))
+
+
+  # Primary INDUSTRY aggregates
+  pfu_aggs_industry <- UKEnergy2000mats |>
+    tidyr::pivot_wider(names_from = matrix.name, values_from = matrix) |>
+    pfu_aggregates(p_industries = p_industries, by = "Industry")
+
+  pfu_aggs_industry |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "E",
+                  .data[[Recca::psut_cols$last_stage]] == "Final") |>
+    magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
+    magrittr::extract2(1) |>
+    expect_equal(matrix(c(50000, 43000), nrow = 1, dimnames = list("Product", c("Resources [of Crude]", "Resources [of NG]"))) |>
+                   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"))
+  pfu_aggs_industry |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "E",
+                  .data[[Recca::psut_cols$last_stage]] == "Useful") |>
+    magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
+    magrittr::extract2(1) |>
+    expect_equal(matrix(c(50000, 43000), nrow = 1, dimnames = list("Product", c("Resources [of Crude]", "Resources [of NG]"))) |>
+                   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"))
+  pfu_aggs_industry |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "E",
+                  .data[[Recca::psut_cols$last_stage]] == "Services") |>
+    magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
+    magrittr::extract2(1) |>
+    expect_equal(matrix(c(50000, 43000), nrow = 1, dimnames = list("Product", c("Resources [of Crude]", "Resources [of NG]"))) |>
+                   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"))
+  pfu_aggs_industry |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "X",
+                  .data[[Recca::psut_cols$last_stage]] == "Services") |>
+    magrittr::extract2(Recca::aggregate_cols$gross_aggregate_primary) |>
+    magrittr::extract2(1) |>
+    expect_equal(matrix(c(53500, 44720), nrow = 1, dimnames = list("Product", c("Resources [of Crude]", "Resources [of NG]"))) |>
+                   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"))
 })
