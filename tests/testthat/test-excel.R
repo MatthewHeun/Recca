c7aedf2c14beb5efaf751dca3f66a243eeb115d9
MatthewHeun <matthew.heun@me.com>
2025-06-05 20:13:27 -0400

    Added a test for regions.

--- tests/testthat/test-excel.R
+++ tests/testthat/test-excel.R
@@ -1,11 +1,25 @@
 test_that("write_ecc_to_excel() works as expected", {
-  ecc <- UKEnergy2000mats %>%
+  ecc <- UKEnergy2000mats |>
     tidyr::spread(key = "matrix.name", value = "matrix")
   ecc_temp_path <- tempfile(pattern = "write_excel_ecc_test_file", fileext = ".xlsx")
 
   res <- write_ecc_to_excel(ecc, path = ecc_temp_path, overwrite = TRUE)
 
   expect_true(file.exists(ecc_temp_path))
+
+  # Test that named matrix regions exist in the file
+  regions <- openxlsx::getNamedRegions(ecc_temp_path)
+  expect_equal(regions,
+               c("R_1", "U_1", "V_1", "Y_1", "r_eiou_1",
+                 "U_eiou_1", "U_feed_1", "S_units_1",
+                 "R_2", "U_2", "V_2", "Y_2", "r_eiou_2",
+                 "U_eiou_2", "U_feed_2", "S_units_2",
+                 "R_3", "U_3", "V_3", "Y_3", "r_eiou_3",
+                 "U_eiou_3", "U_feed_3", "S_units_3",
+                 "R_4", "U_4", "V_4", "Y_4", "r_eiou_4",
+                 "U_eiou_4", "U_feed_4", "S_units_4"),
+               ignore_attr = TRUE)
+
   if (file.exists(ecc_temp_path)) {
     file.remove(ecc_temp_path)
   }
