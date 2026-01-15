8e832ca3d4debc38a0d854155d79514dd4cff166
Matthew Heun <matthew.heun@me.com>
2026-01-15 13:23:20 -0500

    Added more tests for irreversibilities.

--- tests/testthat/test-losses_irrev.R
+++ tests/testthat/test-losses_irrev.R
@@ -48,19 +48,82 @@ test_that("calc_exergy_losses_irrev() works as expected", {
 
   # Spot check values with tests.
   # R matrix with exergy values
-  res |>
+  R <- res |>
     dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "X",
                   .data[[Recca::psut_cols$last_stage]] == "Final") |>
     magrittr::extract2("R") |>
-    magrittr::extract2(1) |>
-    expect_equal(matrix(c(53000, 0,
-                          0, 44720), byrow = TRUE, nrow = 2,
-                        dimnames = list(c("Resources [of Crude]", "Resources [of NG]"),
-                                        c("Crude", "NG"))) |>
-                   matsbyname::setrowtype("Industry") |>
-                   matsbyname::setcoltype("Product"))
+    magrittr::extract2(1)
+  expect_equal(R, matrix(c(53000, 0,
+                           0, 44720), byrow = TRUE, nrow = 2,
+                         dimnames = list(c("Resources [of Crude]", "Resources [of NG]"),
+                                         c("Crude", "NG"))) |>
+                 matsbyname::setrowtype("Industry") |>
+                 matsbyname::setcoltype("Product"))
 
+  # V matrix
+  V <- res |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "X",
+                  .data[[Recca::psut_cols$last_stage]] == "Final") |>
+    magrittr::extract2("V") |>
+    magrittr::extract2(1)
+  # Transformation losses column
+  expect_equal(V[, "MTH.200.C -> Transformation losses"],
+               c(203.4238635,
+                 129.4515495,
+                 46.23269625,
+                 767.4627578,
+                 18.4930785,
+                 952.3935428,
+                 1877.047468,
+                 277.3961775,
+                 3587.657229) |>
+                 magrittr::set_names(c("Crude dist.",
+                                       "Diesel dist.",
+                                       "Elect. grid",
+                                       "Gas wells & proc.",
+                                       "NG dist.",
+                                       "Oil fields",
+                                       "Oil refineries",
+                                       "Petrol dist.",
+                                       "Power plants")))
+  # Destroyed exergy column
+  expect_equal(V[, "Destroyed exergy"],
+               c(378.0761365,
+                 241.5484505,
+                 78.76730375,
+                 1390.537242,
+                 33.0069215,
+                 1775.606457,
+                 3497.952532,
+                 517.6038225,
+                 6752.342771) |>
+                 magrittr::set_names(c("Crude dist.",
+                                       "Diesel dist.",
+                                       "Elect. grid",
+                                       "Gas wells & proc.",
+                                       "NG dist.",
+                                       "Oil fields",
+                                       "Oil refineries",
+                                       "Petrol dist.",
+                                       "Power plants")))
 
+  Y <- res |>
+    dplyr::filter(.data[[Recca::psut_cols$energy_type]] == "X",
+                  .data[[Recca::psut_cols$last_stage]] == "Final") |>
+    magrittr::extract2("Y") |>
+    magrittr::extract2(1)
+
+  # Y matrix transformation losses
+  expect_equal(Y["MTH.200.C -> Transformation losses", ],
+               c(0, 0, 7859.558363, 0) |>
+                 magrittr::set_names(c("Irreversibilities", "Residential",
+                                       "Transformation losses", "Transport")))
+
+  # Y matrix exergy destruction
+  expect_equal(Y["Destroyed exergy", ],
+               c(14665.44164, 0, 0, 0) |>
+                 magrittr::set_names(c("Irreversibilities", "Residential",
+                                       "Transformation losses", "Transport")))
 })
 
 
